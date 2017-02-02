a6e9f836f0836e5460ae45690b929322efce5e4f
dn0765244951 <sketharan1996.15@cse.mrt.ac.lk>
2017-02-02 09:44:51 +0530

    h

---
+++ Main.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+import java.util.Scanner;
+/**
+ *
+ * @author Haran
+ */
+public class GIT {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a;
+        int b;
+        String operation;
+        int answer;
+        Scanner scan = new Scanner(System.in);
+        System.out.println("welcome to my calculator");
+        System.out.println("Enter first number :");
+        a = scan.nextInt();
+        System.out.println("Enter Second number :");
+        b = scan.nextInt();
+        System.out.println("Enter operation :");
+        operation = scan.next();
+        
+        if ("+".equals(operation)){
+            answer = add(a,b);
+            System.out.println("Answer is " + answer);
+        }else{
+            System.out.println("Unsupported operation");
+        }
+        
+    }
+    public static int add(int a, int b){
+        return a+b;
+    }
+}
